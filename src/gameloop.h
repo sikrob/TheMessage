c8b06a89e4ab7323f76faee2e72775f71bcfa051
Robert Sikorski <sikorski.rob@gmail.com>
2013-01-23 21:11:00 -0500

    Actually handling input in game loop now. I admit I'm stalling with all these little bits.

--- src/gameloop.h
+++ src/gameloop.h
@@ -22,37 +22,38 @@ void gameloop(outputFlagContainer *outputFlags) {
 
 	playerStateContainer *playerFlags = malloc(sizeof(playerStateContainer));
 
-	// Always start waking up in bed.
+	// Always start waking up in bed with the note falling down.
 	playerFlags->inBedroom = true;
+	playerInBedroomWakeUp();
+	playerInBedroomNoteFall();
 
 	while(true) {
 		if (playerFlags->inBedroom) {
 			playerInBedroom();
 			outputFlags = getUserInput(outputFlags);
-			// 	if (outputFlags->go && outputFlags->hallway)) {
-			//		playerFlags->inBedroom = false;
-			//		playerFlags->inHallway = true;
-			// 	}
+			if (outputFlags->GO && outputFlags->HALLWAY) {
+				playerFlags->inBedroom = false;
+				playerFlags->inHallway = true;
+			}
 		} else if (playerFlags->inHallway) {
 			playerInHallway();
 			outputFlags = getUserInput(outputFlags);
-			// 	if (outputFlags->go && outputFlags->bedroom)) {
-			//		playerFlags->inHallway = false;
-			//		playerFlags->inBedroom = true;
-			// 	} else if (outputFlags->go && outputFlags->kitchen) {
-			// 		playerFlags->inHallway = false;
-			// 		playerFlags->inKitchen = true;
-			// 	}
+			if (outputFlags->GO && outputFlags->BEDROOM) {
+				playerFlags->inHallway = false;
+				playerFlags->inBedroom = true;
+			} else if (outputFlags->GO && outputFlags->KITCHEN) {
+				playerFlags->inHallway = false;
+				playerFlags->inKitchen = true;
+			}
 		} else if (playerFlags->inKitchen) {
 			playerInKitchen();
 			outputFlags = getUserInput(outputFlags);
-			// 	if (outputFlags->go && hallway) {
-			// 		// can't leave the kitchen once you go in
-			// 		// End game if attempt to leave (but not the bad ending.)
-			//	}
+			if (outputFlags->GO && outputFlags->HALLWAY) {
+				// can't leave the kitchen once you go in
+				// End game if attempt to leave (but not the bad ending.)
+			}
 		}
 
-		break;
 	}
 
 	free(playerFlags);
--- src/messages.h
+++ src/messages.h
@@ -36,19 +36,31 @@ void displayPreGameMenu() {
 }
 
 void playerInBedroom() {
-	puts("Player is in bedroom.");
+	puts("\nYour bedroom is small but functional, with a dresser, closet, and bed. No small amount of clothing littered the");
+	puts("floor and there may have been some old food stuffed into a corner. Your door is open and some of the rooms mess");
+	puts("prevents it from closing anyway. A full length mirror is on one wall.");
+}
+
+void playerInBedroomWakeUp() {
+	puts("\nYou wake up in your bedroom, face down on your bed. Your nose feels raw and crusty, and you have to shake your");
+	puts("arm for a while to wake it up as you sit up in your bed. Your clothes are the same ones you were wearing the");
+	puts("day before; apparently you were too tired to be bothered to change into sleepwear.");
+	puts("\nThe day before was far from your best. After having dinner with your parents, you came home and mused about how");
+	puts("your life was pretty well down the shitter, and between the people who called themselves friends but didn't");
+	puts("return calls and the people who you saw everyday at the exact same job they had been doing for ten or more");
+	puts("years, you had lost a fair amount of faith that things were getting better.");
 }
 
 void playerInBedroomNoteFall() {
-	puts("meow, a note falls down!");
+	puts("\nYou notice from the corner of an eye a folded note falling down from your bed onto the floor.");
 }
 
 void playerInHallway() {
-	puts("Player is in hallway.");
+	puts("\nPlayer is in hallway.");
 }
 
 void playerInKitchen() {
-	puts("Player is in kitchen.");
+	puts("\nPlayer is in kitchen.");
 }
 
 #endif
